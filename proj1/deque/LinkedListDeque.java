dad3104e10f13811b0a315958327ca5f6e99dd90
Maharta <gedemaharta.dev@gmail.com>
2023-03-11 14:03:55 +0800

    Implemented getRecursive for LinkedListDeque

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,5 +1,7 @@
 package deque;
 
+import java.util.Objects;
+
 public class LinkedListDeque<T> {
 
     private class Node {
@@ -61,7 +63,7 @@ public class LinkedListDeque<T> {
 
     public T get(int index) {
         if (index >= size || index < 0) {
-            throw new IndexOutOfBoundsException("index out of bounds.");
+            throw new IndexOutOfBoundsException("index is out of bounds.");
         }
         Node ptr;
         if (index < size / 2) {
@@ -78,6 +80,42 @@ public class LinkedListDeque<T> {
             }
         }
         return ptr.value;
+    }
+
+    /**
+     * Get the value at index with recursion.
+     */
+    public T getRecursive(int index) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("index is out of bounds.");
+        }
+        if (index < size / 2) {
+            return getRecursive(index, sentinel.next, "beginning");
+        } else {
+            return getRecursive(size - 1 - index, sentinel.prev, "end");
+        }
+    }
+
+    /**
+     * Get the value at index with recursion.
+     * <p>
+     * Will get passed either the first node or the last node for the second argument.
+     * This depends on whether index is closer to the start or the end of the list.
+     * <p>
+     * Will also get passed "beginning" or "end" String for the last argument.
+     * This will help the helper function whether to traverse to the front or to the back.
+     * <p>
+     * index will determine how many times is traversing needed for the node pointer.
+     */
+    private T getRecursive(int index, Node node, String startFrom) {
+        if (index == 0) {
+            return node.value;
+        }
+        if (Objects.equals(startFrom, "beginning")) {
+            return getRecursive(index - 1, node.next, startFrom);
+        } else {
+            return getRecursive(index - 1, node.prev, startFrom);
+        }
     }
 
     public boolean equals(Object o) {
@@ -131,6 +169,10 @@ public class LinkedListDeque<T> {
         System.out.println(list.get(1));
         System.out.println(list.get(2));
         System.out.println(list.get(3));
+        System.out.println(list.getRecursive(0));
+        System.out.println(list.getRecursive(1));
+        System.out.println(list.getRecursive(2));
+        System.out.println(list.getRecursive(3));
         System.out.println(list.isEmpty());
         list.removeLast();
         list.removeFirst();
