a843c4245c76288a167f14eeee72425de4fdca2e
Gede Maharta Pamuji <gedemaharta.dev@gmail.com>
2023-05-30 13:19:30 +0800

    checkout correct implementation from the past and commited

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,18 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Maharta Pamuji
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** A function to return nextNumber
+     * of Collatz sequence.
+     * @param n */
     public static int nextNumber(int n) {
         if (n  == 128) {
             return 1;
         } else if (n == 5) {
             return 3 * n + 1;
         } else {
-            return n * 2;
+            return n / 2;
         }
     }
 
